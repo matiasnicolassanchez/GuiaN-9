dc59f64bf04eb1b9e07ea3f30f2093148eda37a9
Matias <smatiias@gmail.com>
2022-10-20 19:48:28 -0300

    Quinto commit desde pycharm (Ej4 - Terminado)

--- Ej4.py
+++ Ej4.py
@@ -1,12 +1,12 @@
-botellas_nec = int(input("Ingrese la cantidad de botellas que se necesitan reciclar para obtener una nueva: "))
+botellas_nec = int(input("Ingrese la cantidad de botellas que se necesitan para obtener una nueva: "))
 botellas_reciclar = int(input("Ingrese la cantidad de botellas a reciclar: "))
-nuevas = (botellas_reciclar // botellas_nec)
-sobrante1 = (botellas_reciclar % botellas_nec)
-num_reciclaje = 1
-while sobrante1 != 0:
-    nuevas = ((botellas_reciclar + sobrante1) // botellas_nec)
-    sobrante = ((botellas_reciclar + sobrante1) % botellas_nec)
-    print(f"Reciclaje número {num_reciclaje}: \nBotellas a reciclar:{botellas_reciclar} \nBotellas necesarias para obtener una nueva:{botellas_nec} \nSobrante de reciclaje anterior:{sobrante} \nUsted obtendra: {nuevas} botellas nuevas")
-    sobrante1 = sobrante
-
-if sobrante1 == 0:
+sobrante = (botellas_reciclar % botellas_nec)
+sobran = 0
+reciclaje = 1
+while sobrante != 0:
+    nuevas = ((botellas_reciclar) // botellas_nec)
+    sobran = (botellas_reciclar % botellas_nec)
+    sobrante = sobran
+    botellas_reciclar = (nuevas + sobran)
+    print(f"{reciclaje} reciclaje: Con {botellas_reciclar} botellas a reciclar --> Se fabrican {nuevas} botellas y sobran {sobran} botellas ")
+    reciclaje = (reciclaje + 1)
--- Ej5.py
+++ Ej5.py
@@ -3,9 +3,12 @@ cant_suma = 1
 sum_productos = 0
 multiplicador = 1
 producto = 0
-ubicacion = 0
-while ubicacion < 9:
-    producto = (int(nisbn.index("ubicacion")) * multiplicador)
+ubicacion = str(0)
+digito = str
+while int(ubicacion) < 9:
+    digito = nisbn.index("0")
+
+    producto = (nisbn.index("ubicacion") * multiplicador)
     sum_productos = (sum_productos + producto)
     ubicacion = (ubicacion + 1)
     multiplicador = (multiplicador + 1)
